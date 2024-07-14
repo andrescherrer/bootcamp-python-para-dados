16daaf9e0474262952fd39d69dbeba411065b5a5
André C. Scherrer <andrescherrer@gmail.com>
2024-07-14 06:15:11 -0300

    fix: resolve ex-02 da aula 002

---
+++ aula-002/exercicio-002.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero = int(input("Digite um número inteiro:"))
+resto = numero % 5
+
+print("O resto da divisão por 5 é: ", resto)

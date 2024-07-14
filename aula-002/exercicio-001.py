539705e1f7ed22921417f099323d59d937fece44
André C. Scherrer <andrescherrer@gmail.com>
2024-07-14 06:04:35 -0300

    feat: resolve ex-01 da aula 002

---
+++ aula-002/exercicio-001.py
@@ -0,0 +1,7 @@
+print("Escreva um programa que soma dois números inteiros inseridos pelo usuário.")
+
+n1 = int(input("Digite um número inteiro:"))
+n2 = int(input("Digite outro número inteiro:"))
+resultado = n1 + n2
+
+print("A soma dos números que você digitou é: ", resultado)

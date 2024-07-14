e2b548e42146361f600a645dd22cf8fe36e77817
André C. Scherrer <andrescherrer@gmail.com>
2024-07-14 07:03:00 -0300

    feat: resolve ex-7 da aula-002

---
+++ aula-002/exercicio-007.py
@@ -0,0 +1,8 @@
+# Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+numero_1 = float(input("Digite um número:"))
+numero_2 = float(input("Digite outro número:"))
+
+resultado = (numero_1 + numero_2)/2
+
+print("A média dos números é: ", resultado)

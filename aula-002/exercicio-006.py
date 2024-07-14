7fd07f5a2381450f30be61ed03a29c2f0e0b5c3a
André C. Scherrer <andrescherrer@gmail.com>
2024-07-14 06:47:34 -0300

    feat: resolve ex-6 da aula-002

---
+++ aula-002/exercicio-006.py
@@ -0,0 +1,8 @@
+# Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+numero_1 = float(input("Digite um número:"))
+numero_2 = float(input("Digite outro número:"))
+
+resultado = numero_1 + numero_2
+
+print("A soma dos números é: ", resultado)

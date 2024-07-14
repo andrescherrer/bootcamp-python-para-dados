86387acd36a6b3d8265a7e07ea4c34b0f0945e7e
André C. Scherrer <andrescherrer@gmail.com>
2024-07-14 06:39:00 -0300

    feat: resolve ex-4 da aula-002

---
+++ aula-002/exercicio-004.py
@@ -0,0 +1,7 @@
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+numero_1 = int(input("Digite um número inteiro:"))
+numero_2 = int(input("Digite outro número inteiro:"))
+resultado = numero_1 // numero_2
+
+print("O resultado da divisao inteira dos números é: ", resultado)

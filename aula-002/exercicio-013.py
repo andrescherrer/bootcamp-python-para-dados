1e6f8bb6fe807cadd36f9a024d8775b948cb5400
André C. Scherrer <andrescherrer@gmail.com>
2024-07-15 06:06:20 -0300

    feat: resolve ex-13 da aula-002

---
+++ aula-002/exercicio-013.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+import math
+
+print("Remove espaços no começo e final do texto")
+
+frase = input("Entre com uma frase")
+
+print("Texto convertivo para minúsculas: ", frase.strip())
